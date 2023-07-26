09645d8cfb73d79926cd6874f64b9d8c93cefb51
Ariadna Frias <ariadna.812@gmail.com>
2023-07-26 17:21:29 +0200

    Update styles in card and header

--- src/components/app.router/app.router.jsx
+++ src/components/app.router/app.router.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import { HomePage } from "../pages/homePage";
+import { HomePage } from "../pages/homePage/homePage";
 
 export function AppRouter() {
   return (
--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import "./card.scss";
 
 Card.propTypes = {
@@ -9,7 +10,9 @@ export function Card({ movie }) {
   const image = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
   return (
     <li className="card">
-      <img src={image} alt={movie.title} className="card__image"></img>
+      <Link to={`/details/${movie.id}`}>
+        <img src={image} alt={movie.title} className="card__image"></img>
+      </Link>
       <div className="card__title">{movie.title}</div>
     </li>
   );
