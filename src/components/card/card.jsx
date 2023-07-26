41fa599f7c88201f54d0405750dc45050d0e87fe
Ariadna Frias <ariadna.812@gmail.com>
2023-07-26 13:02:05 +0200

    Update card element and style

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,15 +1,16 @@
 import PropTypes from "prop-types";
+import "./card.scss";
 
 Card.propTypes = {
-  movie: PropTypes.element.isRequired,
+  movie: PropTypes.object,
 };
 
 export function Card({ movie }) {
   const image = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
   return (
-    <div>
-      <img src={image} alt={movie.title}></img>
-      <li>{movie.title}</li>
-    </div>
+    <li className="card">
+      <img src={image} alt={movie.title} className="card__image"></img>
+      <div className="card__title">{movie.title}</div>
+    </li>
   );
 }
