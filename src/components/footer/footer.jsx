988cf75f37dbf38f8b25904d67916233eea38ee9
Ariadna Frias <ariadna.812@gmail.com>
2023-07-30 21:01:32 +0200

    Update styles in header and footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,11 @@
+import "./footer.scss";
+
+export function Footer() {
+  return (
+    <>
+      <footer className="footer">
+        <span className="footer__title">Data provided by The Movie DB API</span>
+      </footer>
+    </>
+  );
+}
