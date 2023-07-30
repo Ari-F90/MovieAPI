6218ae8e9cf47408e32ba53856945fe47a71f6c5
Ariadna Frias <ariadna.812@gmail.com>
2023-07-30 21:01:13 +0200

    Update app and usemovies custom hook

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,11 +1,13 @@
 import { AppRouter } from "../components/app.router/app.router";
 import { Header } from "../components/header/header";
+import { Footer } from "../components/footer/footer";
 
 function App() {
   return (
     <>
       <Header></Header>
       <AppRouter></AppRouter>
+      <Footer></Footer>
     </>
   );
 }
